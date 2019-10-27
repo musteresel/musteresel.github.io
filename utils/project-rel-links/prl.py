9e35b707cc09ee4338ca2f47ee50adcaa3b342c5
Daniel Jour <musteresel@gmail.com>
2019-10-28 00:25:08 +0100

    prl.py: Extract pathToProjectRoot from meta only once

--- utils/project-rel-links/prl.py
+++ utils/project-rel-links/prl.py
@@ -2,13 +2,21 @@
 
 from pandocfilters import toJSONFilter, Image, Link
 
+ptpr = None;
+
+def SetPathToProjectRoot(meta):
+  global ptpr
+  if ptpr is None:
+    result = meta.get("pathToProjectRoot")
+    if 'MetaInlines' in result['t']:
+        assert result['c']['t'] == 'Str'
+        ptpr = result['c']['c']
+    elif 'MetaString' in result['t']:
+        ptpr = result['c']
+
 def prl(key, value, format, meta):
-  result = meta.get("pathToProjectRoot")
-  if 'MetaInlines' in result['t']:
-      assert result['c']['t'] == 'Str'
-      ptpr = result['c']['c']
-  elif 'MetaString' in result['t']:
-      ptpr = result['c']
+  SetPathToProjectRoot(meta)
+  global ptpr
   if key == 'Image' and value[2][0].startswith("/"):
     value[2][0] = ptpr + value[2][0]
     return Image(*value)
